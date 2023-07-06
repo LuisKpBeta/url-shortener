d9351e81e8bd4aae919cead08d65395fa2d3f750
Luis <lu.ca.oliveira41@gmail.com>
2023-07-06 00:40:36 -0400

    feat: setup gin routes for metrics

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"log"
+
 	"github.com/LuisKpBeta/url-shortener/internal/api"
 	"github.com/LuisKpBeta/url-shortener/internal/database"
+	"github.com/LuisKpBeta/url-shortener/internal/prometheus"
 	"github.com/LuisKpBeta/url-shortener/internal/token"
 	"github.com/LuisKpBeta/url-shortener/pk/repository"
 	service_url "github.com/LuisKpBeta/url-shortener/pk/service/url"
@@ -11,6 +14,10 @@ import (
 func main() {
 	conn := database.ConnectToDatabase()
 
+	prometheusService, err := prometheus.NewPrometheusService()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 	createAction := repository.CreateUrl(conn)
 	parameters := service_url.CreateParameters{
 		CreateToken:       token.GenerateUrlToken,
@@ -23,8 +30,11 @@ func main() {
 	}
 
 	server := api.CreateHttpServer()
+	api.CreatePrometheusHandler(server)
+	api.CreateMetricsMiddleware(server, prometheusService)
 	api.GetUrlByTokenHandler(server, service_url.ReadUrl(readTokenParameters))
 	api.CreateUrlShortnerHandler(server, service_url.Create(parameters))
+
 	api.StartHttpServer(server)
 
 }
